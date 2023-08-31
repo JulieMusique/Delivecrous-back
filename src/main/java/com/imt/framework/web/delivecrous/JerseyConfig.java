56e3a08627939a54ab542a8933af103097cf4876
ALX <110174531+AlexisEgea@users.noreply.github.com>
2023-08-31 14:17:39 +0200

    adding new config Jersey for dish (only for now)

--- src/main/java/com/imt/framework/web/delivecrous/JerseyConfig.java
+++ src/main/java/com/imt/framework/web/delivecrous/JerseyConfig.java
@@ -1,28 +1,18 @@
-/**
- * 
- */
 package com.imt.framework.web.delivecrous;
 
+
+import com.imt.framework.web.delivecrous.ressources.DishResource;
+import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-import com.imt.framework.web.delivecrous.ressources.LivreResource;
-
-import jakarta.ws.rs.ApplicationPath;
-
-/**
- * @author julie.catteau@etu.imt-nord-europe.fr
- * @date 29 août 2023
- * Le but de ce fichier et de référencer les classes dont les méthodes vont être exposées sur le serveur embarqué
- * Ce code va être exécuté au démarrage de l'application
- */
 @Component
-@ApplicationPath("demo")
+@ApplicationPath("DelivCROUS")
 @Configuration
-public class JerseyConfig extends ResourceConfig{
-	public JerseyConfig() {
-		register(LivreResource.class);
-	}
+public class JerseyConfig extends ResourceConfig {
+    public JerseyConfig(){
+        register(DishResource.class);
+    }
 
 }
