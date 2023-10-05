1c1c9ab788796433f4f4288980340faceb00f8fd
DavidIMT <david.dupont@imt-nord-europe.fr>
2023-10-05 09:31:54 +0200

    fix références des packages du CORSFilter

--- src/main/java/com/imt/framework/web/delivecrous/JerseyConfig.java
+++ src/main/java/com/imt/framework/web/delivecrous/JerseyConfig.java
@@ -1,6 +1,6 @@
 package com.imt.framework.web.delivecrous;
 
-import com.project.cors.CorsFilter;
+import com.imt.framework.web.delivecrous.cors.CorsFilter;
 import com.imt.framework.web.delivecrous.resources.DishResource;
 import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
