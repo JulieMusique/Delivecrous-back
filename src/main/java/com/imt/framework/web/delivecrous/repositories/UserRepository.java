9fdb2894ef6644117618cc8181f302d0e4faadcf
fathounesalma <143490414+fathounesalma@users.noreply.github.com>
2023-10-10 11:31:24 +0200

    Update UserRepository

--- src/main/java/com/imt/framework/web/delivecrous/repositories/UserRepository.java
+++ src/main/java/com/imt/framework/web/delivecrous/repositories/UserRepository.java
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-// Source code is decompiled from a .class file using FernFlower decompiler.
 package com.imt.framework.web.delivecrous.repositories;
 
 import com.imt.framework.web.delivecrous.entities.User;
@@ -12,37 +9,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("select l from User l where l.id = :identifiant")
+    @Query(value="select * from users l where l.id = :identifiant",nativeQuery = true)
     List<User> getUserWithIdFilter(@Param("identifiant") Long identifiant);
 
-    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.email = ?1")
-    boolean existsByEmail(String email);
+    @Query(value="SELECT CASE WHEN COUNT(*) > 0 THEN true ELSE false END FROM users u WHERE u.email = :email", nativeQuery = true)
+    boolean existsByEmail(@Param("email") String email);
 
     @Query(value = "SELECT * FROM users u WHERE u.email = :email", nativeQuery = true)
     User findByEmail(@Param("email") String email);
 
-=======
-
-// Source code is decompiled from a .class file using FernFlower decompiler.
-package com.imt.framework.web.delivecrous.repositories;
-
-import com.imt.framework.web.delivecrous.entities.User;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("select l from User l where l.id = :identifiant")
-    List<User> getUserWithIdFilter(@Param("identifiant") Long identifiant);
-
-    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.email = ?1")
-    boolean existsByEmail(String email);
-
-    @Query(value = "SELECT * FROM users u WHERE u.email = :email", nativeQuery = true)
-    User findByEmail(@Param("email") String email);
-
->>>>>>> e46c969791cf21b85adbf526abec9adcd779a1a6
-}
